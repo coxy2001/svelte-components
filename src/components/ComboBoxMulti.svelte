9395ea7e1c18ce402b73040450aeed6788703ea5
Nathan Cox <64635910+coxy2001@users.noreply.github.com>
2024-06-18 11:12:38 +1200

    Update ComboBoxMulti.svelte

--- src/components/ComboBoxMulti.svelte
+++ src/components/ComboBoxMulti.svelte
@@ -68,7 +68,7 @@
     }
 
     function prev() {
-        if (highlightedIndex > 0) {
+        if (highlightedIndex > 0 && highlightedIndex < filteredItems.length) {
             highlightedIndex--;
         } else {
             highlightedIndex = filteredItems.length - 1;
