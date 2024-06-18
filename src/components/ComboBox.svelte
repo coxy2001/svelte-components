63a30dac669f7ac648e9eaec48f46545bd7732e7
Nathan Cox <64635910+coxy2001@users.noreply.github.com>
2024-06-19 11:48:44 +1200

    Fix ComboBox enter

--- src/components/ComboBox.svelte
+++ src/components/ComboBox.svelte
@@ -49,7 +49,7 @@
                 expand();
             } else if (
                 highlightedIndex > -1 &&
-                highlightedIndex < filteredItems.length - 1
+                highlightedIndex < filteredItems.length
             ) {
                 select(highlightedIndex);
             } else if (search) {
